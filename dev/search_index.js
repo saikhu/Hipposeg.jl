8ca0cefc0a602338017ff1470f5471dfd31fd1ed
Documenter.jl <documenter@juliadocs.github.io>
2022-04-26 05:48:12 +0000

    build based on de144df

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Hipposeg","category":"page"},{"location":"#Hipposeg","page":"Home","title":"Hipposeg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Hipposeg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Hipposeg]","category":"page"}]
+}
